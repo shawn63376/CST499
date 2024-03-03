40b1be20dbc9bca70c51c95eb9ac8eb50a2456e0
shawn63376 <77697432+shawn63376@users.noreply.github.com>
2024-03-03 13:43:15 -0600

    Add files via upload
    These are the PHP files. SQL file and PPX Presentation for CST499 Final Project by Shawn Smith on March 4, 2024

---
+++ student-portal/connectDB.php
@@ -0,0 +1,63 @@
+<?php
+
+class connectDB {
+  
+  function executeSelectQuery($con,$sql){
+    
+    $result = mysqli_query($con, $sql); 
+    echo "<br>";
+    echo "<table border='1'>";
+    while ($row = mysqli_fetch_assoc($result)) { 
+        echo "<tr>";
+        foreach ($row as $field => $value) { 
+            echo "<td>" . $value . "</td>"; 
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+    
+  }
+  
+  function executeQuery($con,$sql){
+    
+    if ($con->connect_error) {
+        die("Connection failed: " . $con->connect_error);
+    }
+
+    if ($con->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $con->error;
+    }
+    
+    $con->close();
+
+    
+  }
+  
+  
+}
+
+/*--$serverName = "localhost";
+$userName= "root";
+$password = "";
+$dbName="cst310";
+
+//create Connection
+
+$con = mysqli_connect($serverName,$userName,$password,$dbName);
+
+$sql = "SELECT id, email, password, firstName, lastName, address, phone, salary, ssn FROM tbluser";
+
+$connect1 = new connectDB();
+
+$connect1->executeSelectQuery($con,$sql);
+
+$sql2 = "INSERT INTO tbluser (id, email, password, firstName, lastName, address, phone, salary, ssn)
+VALUES (3, 'anders@gmail.com', 'password2','anders','berg','123 street',999999,0,0)";
+
+$connect1->executeQuery($con,$sql2);*/
+
+
+?>
+
